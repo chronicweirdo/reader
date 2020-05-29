2774ea79eadf5ebfd7fea966e9fede929ffed470
account@cacoveanu.com <account@cacoveanu.com>
2020-05-29 09:07:46 +0300

    a first search implemented, now working

--- src/main/scala/com/cacoveanu/reader/repository/ComicRepository.scala
+++ src/main/scala/com/cacoveanu/reader/repository/ComicRepository.scala
@@ -10,6 +10,6 @@ trait ComicRepository extends JpaRepository[DbComic, java.lang.Long] {
 
   def findAllByOrderByCollectionAsc(pageable: Pageable): java.util.List[DbComic]
 
-  @Query(value="select * from db_comic c where c.title like '%term%' or c.collection like '%term%'", nativeQuery = true)
+  @Query(value="select * from db_comic c where lower(c.title) like %:term% or lower(c.collection) like %:term%", nativeQuery = true)
   def search(@Param("term") term: String): java.util.List[DbComic]
 }
