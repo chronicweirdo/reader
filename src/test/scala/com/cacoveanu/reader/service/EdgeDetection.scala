fc555f387b87afacda405be9a7742a850c5bf65f
account@cacoveanu.com <account@cacoveanu.com>
2020-06-23 16:46:31 +0300

    playing around with sobel

--- src/test/scala/com/cacoveanu/reader/service/EdgeDetection.scala
+++ src/test/scala/com/cacoveanu/reader/service/EdgeDetection.scala
@@ -4,6 +4,7 @@ import java.awt.Graphics
 import java.awt.image.{BufferedImage, BufferedImageOp, ConvolveOp, Kernel}
 import java.io.{BufferedOutputStream, ByteArrayInputStream, ByteArrayOutputStream, FileOutputStream}
 import java.nio.file.{Files, Paths}
+import java.awt.Graphics2D
 
 import javax.imageio.ImageIO
 
@@ -16,6 +17,27 @@ class BlurFilter extends MyFilter {
     val blurMatrix = Array(5.0f / 9.0f, 5.0f / 9.0f, 5.0f / 9.0f, 5.0f / 9.0f, 5.0f / 9.0f, 5.0f / 9.0f, 5.0f / 9.0f, 5.0f / 9.0f, 5.0f / 9.0f)
     val blurFilter: ConvolveOp = new ConvolveOp(new Kernel(3, 3, blurMatrix), ConvolveOp.EDGE_NO_OP, null);
     blurFilter.filter(image, null)
+  }
+}
+
+class GaussianFilter extends MyFilter {
+  override def process(image: BufferedImage): BufferedImage = {
+    val matrix = Array(
+      2f/159, 4f/159, 5f/159, 4f/159, 2f/159,
+      4f/159, 9f/159, 12f/159, 9f/159, 4f/159,
+      5f/159, 12f/159, 15f/159, 12f/159, 5f/159,
+      4f/159, 9f/159, 12f/159, 9f/159, 4f/159,
+      2f/159, 4f/159, 5f/159, 4f/159, 2f/159
+    )
+    val filter = new ConvolveOp(new Kernel(5, 5, matrix), ConvolveOp.EDGE_NO_OP, null)
+    filter.filter(image, null)
+  }
+}
+
+class CustomFilter(width: Int, height: Int, matrix: Array[Float]) extends MyFilter {
+  override def process(image: BufferedImage): BufferedImage = {
+    val filter = new ConvolveOp(new Kernel(width, height, matrix), ConvolveOp.EDGE_NO_OP, null)
+    filter.filter(image, null)
   }
 }
 
@@ -37,8 +59,31 @@ object EdgeDetection {
     bos.close()
   }
 
+  def imageToFile(image: BufferedImage, path: String) = {
+    bytesToFile(imageToBytes(image), path)
+  }
+
+  def toBW(img: BufferedImage): BufferedImage = {
+    val out = new BufferedImage(img.getWidth(), img.getHeight(), BufferedImage.TYPE_BYTE_GRAY)
+    val g = out.createGraphics
+    g.drawImage(img, 0, 0, null)
+    out
+  }
+
+  def merge(img1: BufferedImage, img2: BufferedImage): BufferedImage = {
+    val out = new BufferedImage(
+      Math.min(img1.getWidth(), img2.getWidth()),
+      Math.min(img2.getHeight(), img2.getHeight()),
+      BufferedImage.TYPE_INT_RGB
+    )
+    val g = out.createGraphics
+    g.drawImage(img1, 0, 0, null)
+    g.drawImage(img2, 0, 0, null)
+    out
+  }
+
   def main(args: Array[String]): Unit = {
-    val inputImagePath = "C:\\Users\\silvi\\Desktop\\Howard the Duck 006-005 (AnPymGold-Empire).jpg"
+    val inputImagePath = "input.jpg"
 
     val image: BufferedImage = loadImage(inputImagePath)
 
@@ -48,16 +93,22 @@ object EdgeDetection {
       }
     }*/
 
-    val blurFilter = new BlurFilter
-    val blurredImage = blurFilter.process(image)
+    val gaussianFilter = new GaussianFilter
+    val blurredImage = gaussianFilter.process(image)
+    val bwImage = toBW(blurredImage)
 
+    imageToFile(bwImage, "bw.jpg")
+    val sobelHorizontal = new CustomFilter(3, 3, Array(1f, 2f, 1f, 0, 0, 0, -1f, -2f, -1f))
+    imageToFile(sobelHorizontal.process(bwImage), "s1.jpg")
+    val sobelVertical = new CustomFilter(3, 3, Array(1f, 0, -1, 2, 0, -2, 1, 0, -1))
+    imageToFile(sobelVertical.process(bwImage), "s2.jpg")
 
     //val g: Graphics = image.getGraphics
 
     //out.toByteArray
 
-    val outputFilePath = "test.jpg"
-    bytesToFile(imageToBytes(blurredImage), outputFilePath)
+    /*val outputFilePath = "test.jpg"
+    bytesToFile(imageToBytes(blurredImage), outputFilePath)*/
   }
 
 }
