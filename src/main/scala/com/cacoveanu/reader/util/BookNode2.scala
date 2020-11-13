f02bd6f524804098cedc1c7169c3e611aa84c74d
account@cacoveanu.com <account@cacoveanu.com>
2020-11-13 19:24:51 +0200

    computing positions

--- src/main/scala/com/cacoveanu/reader/util/BookNode2.scala
+++ src/main/scala/com/cacoveanu/reader/util/BookNode2.scala
@@ -1,19 +1,23 @@
 package com.cacoveanu.reader.util
 
+import com.cacoveanu.reader.util.BookNode2.isLeafElement
+
 object BookNode2 {
 
   private val VOID_ELEMENTS = Seq("area","base","br","col","hr","img","input","link","meta","param","keygen","source")
+  private val LEAF_ELEMENTS = Seq("img")
 
   // if it starts and ends with angle brackets
   def isTag(str: String) = "^</?[^>]+>$".r matches str
   def isEndTag(str: String) = "^</[^>]+>$".r matches str
   def isBothTag(str: String) = "^<[^>/]+/>$".r matches str
-  def tagName(str: String) = "</?([^>\\s]+)".r findFirstMatchIn str match {
+  def getTagName(str: String) = "</?([^>\\s]+)".r findFirstMatchIn str match {
     case Some(m) =>
       m.group(1)
     case None => null
   }
-  def voidElement(tagName: String) = VOID_ELEMENTS.contains(tagName.toLowerCase())
+  def isVoidElement(tagName: String) = VOID_ELEMENTS.contains(tagName.toLowerCase())
+  def isLeafElement(tagName: String) = LEAF_ELEMENTS.contains(tagName.toLowerCase())
 
   def parse(body: String): Option[BookNode2] = {
     val bodyNode = new BookNode2("body", null)
@@ -43,11 +47,11 @@ object BookNode2 {
       if (c == '>') {
         // ending a tag
         if (isTag(content)) {
-          val name = tagName(content)
+          val name = getTagName(content)
           // can only be a tag
           if (isEndTag(content)) {
             // we check that this tag closes the current node correctly
-            if (voidElement(name)) {
+            if (isVoidElement(name)) {
               // the last child should have the correct name
               if (name != current.children.last.name) throw new Throwable("incompatible end for void tag")
               else {
@@ -59,7 +63,7 @@ object BookNode2 {
               // move current node up
               current = current.parent
             }
-          } else if (isBothTag(content) || voidElement(name)) {
+          } else if (isBothTag(content) || isVoidElement(name)) {
             // just add this tag without content
             current.addChild(new BookNode2(name, content))
           } else {
@@ -74,6 +78,7 @@ object BookNode2 {
       }
     }
 
+    bodyNode.updatePositions()
     Some(bodyNode)
   }
 }
@@ -93,6 +98,8 @@ class BookNode2 {
     this.content = content
   }
 
+  def getLength() = this.end - this.start + 1
+
   def addChild(child: BookNode2) = {
     children = children :+ child
     child.parent = this
@@ -105,5 +112,28 @@ class BookNode2 {
   def prettyPrint(level: Int = 0): Unit = {
     printAtLevel(level, this.name + "[" + this.start + "," + this.end + "]: " + this.content)
     this.children.foreach(c => c.prettyPrint(level+1))
+  }
+
+  private def updatePositions(entrancePosition: Int = 0): Unit = {
+    var position = entrancePosition
+    this.start = position
+    if (this.name == "text") {
+      this.end = this.start + this.content.length - 1
+    } else if (isLeafElement(this.name)) {
+      // occupies a single position
+      this.end = this.start
+    } else if (this.children.isEmpty) {
+      // an element without children, maybe used for spacing, should occupy a single position
+      this.end = this.start
+    } else {
+      // compute for children and update
+      for (i <- this.children.indices) {
+        val child = this.children(i)
+        child.updatePositions(position)
+        position = child.end + 1
+      }
+      this.end = this.children.last.end
+    }
+
   }
 }
