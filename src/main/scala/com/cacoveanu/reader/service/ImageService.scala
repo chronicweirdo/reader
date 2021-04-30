6ccb00faf6ab4eabdf30892b53cb2d2cce3bcdb9
account@cacoveanu.com <account@cacoveanu.com>
2021-04-30 14:03:14 +0300

    generate a cover image

--- src/main/scala/com/cacoveanu/reader/service/ImageService.scala
+++ src/main/scala/com/cacoveanu/reader/service/ImageService.scala
@@ -1,16 +1,103 @@
 package com.cacoveanu.reader.service
 
 import java.awt.image.BufferedImage
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
-
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, File}
 import com.cacoveanu.reader.util.FileUtil
+
 import javax.imageio.ImageIO
 import org.springframework.stereotype.Service
 
+import java.awt
+import java.awt.Color
 import scala.collection.mutable
+import java.awt.Font
+import java.awt.FontFormatException
+import java.awt.GraphicsEnvironment
+import java.io.IOException
+import scala.util.Random
 
 @Service
 class ImageService {
+
+  // install fonts
+  try {
+    val ge = GraphicsEnvironment.getLocalGraphicsEnvironment
+    ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, this.getClass.getResourceAsStream("/static/Merriweather/Merriweather-Regular.ttf")))
+  } catch {
+    case e: Throwable =>
+      e.printStackTrace()
+  }
+
+  private def hex2Rgb(colorStr: String) =
+    Color(
+      Integer.valueOf(colorStr.substring(1, 3), 16),
+      Integer.valueOf(colorStr.substring(3, 5), 16),
+      Integer.valueOf(colorStr.substring(5, 7), 16)
+    )
+
+  private def toGraphicsColor(color: Color) = new awt.Color(color.red, color.green, color.blue)
+
+  private def pickAccentColor(): Color = {
+    val options = Seq(hex2Rgb("#FEC5BB"),
+      hex2Rgb("#9bf6ff"), hex2Rgb("#a0c4ff"), hex2Rgb("#caffbf"), hex2Rgb("#fdffb6"),
+      hex2Rgb("#ffadad"), hex2Rgb("#ccd5ae"), hex2Rgb("#d4a373"))
+    options(Random.nextInt(options.size))
+  }
+
+  def generateCover(title: String, accentColor: Color = pickAccentColor()): Array[Byte] = {
+    val height = 700
+    val width = (height * .6).toInt
+    val margin = (height * .05).toInt
+
+    val generatedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+    val graphics = generatedImage.createGraphics
+    val serifFont = new Font("Merriweather", Font.PLAIN, 50)
+    graphics.setFont(serifFont)
+    val serifFontMetrics = graphics.getFontMetrics(serifFont)
+
+    var lines = Seq[String]()
+    val tokens = title.split("\\s")
+    var currentLine = ""
+    for (token <- tokens) {
+      // add token to line
+      if (currentLine.length > 0) currentLine = currentLine + " "
+      currentLine = currentLine + token
+      // check size of line
+      if (serifFontMetrics.stringWidth(currentLine) > width - 4 * margin) {
+        // remove last token
+        currentLine = currentLine.substring(0, currentLine.lastIndexOf(" "))
+        // save line
+        lines = lines :+ currentLine
+        // generate new line
+        currentLine = token
+      }
+    }
+    if (currentLine.length > 0) {
+      lines = lines :+ currentLine
+    }
+
+    // draw lines
+    var lineHeight = margin
+    for (line <- lines) {
+      graphics.drawString(line, margin*2, lineHeight + serifFontMetrics.getHeight())
+      lineHeight = lineHeight + serifFontMetrics.getHeight()
+    }
+
+    // draw rectangles over edges to hide "overflow"
+    graphics.setColor(toGraphicsColor(accentColor))
+    graphics.fillRect(0, 0, margin, height)
+    graphics.fillRect(0, 0, width, margin)
+    graphics.fillRect(0, height-margin, width, margin)
+    graphics.fillRect(width-margin, 0, margin, height)
+
+    graphics.dispose()
+
+    //ImageIO.write(generatedImage, "jpeg", new File("test.jpg"))
+
+    val out = new ByteArrayOutputStream()
+    ImageIO.write(generatedImage, "jpeg", out)
+    out.toByteArray
+  }
 
   private def resizeByFactor(factor: Double, originalWidth: Int, originalHeight: Int) =
     ((originalWidth * factor).floor.toInt, (originalHeight * factor).floor.toInt)
