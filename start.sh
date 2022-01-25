80fc6df1ba73f99fbf363c3d77da9b6cf301c39b
account@cacoveanu.com <account@cacoveanu.com>
2022-01-25 08:38:31 +0200

    remove parallelization options for scan, not helping, disk access (windows+docker) is bottleneck

--- start.sh
+++ start.sh
@@ -2,6 +2,5 @@ sed -i 's/ADMIN_PASSWORD/'$ADMIN_PASSWORD'/' application.properties
 sed -i 's/SERVER_PORT/'$SERVER_PORT'/' application.properties
 sed -i 's/DEBUG/'$DEBUG'/' application.properties
 sed -i 's/LOG_LEVEL/'$LOG_LEVEL'/' application.properties
-sed -i 's/SCANNER_WORKERS/'$SCANNER_WORKERS'/' application.properties
 
-java -XX:ActiveProcessorCount=$PROCESSORS -Dcom.sun.media.imageio.disableCodecLib=true -jar reader.jar
+java -Dcom.sun.media.imageio.disableCodecLib=true -jar reader.jar
